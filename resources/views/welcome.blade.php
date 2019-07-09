e69d9e2cc746423d9b50ca281fe77087dac6b9d8
vaniaparamita <vania.parammita23@gmail.com>
2019-07-09 10:54:49 +0700

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -100,7 +100,8 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
+                    
+                <a href="{{ url('/login') }}">Login</a>
                     <a href="{{ url('/register') }}">Register</a>
                 </div>
             @endif
