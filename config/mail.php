542005043b155fb87540d148bee1e0353d819103
Muhammad Dzakwan Zaky <dzakwanzaky48@gmail.com>
2019-08-03 13:42:47 +0700

    reset password

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.googlemail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 465),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => env('MAIL_FROM_ADDRESS', 'hris.jcm@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'No-reply'),
     ],
 
     /*
@@ -71,7 +71,7 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
 
     /*
     |--------------------------------------------------------------------------
@@ -110,6 +110,11 @@ return [
     |
     */
 
-    'sendmail' => '/usr/sbin/sendmail -bs',
-
+'markdown' => [
+        'theme' => 'default',
+ 
+        'paths' => [
+            resource_path('views/vendor/mail'),
+        ],
+    ],
 ];
